aa06b0239864d250fa97d8694c12d7b1d84966b6
Walid <walid-kebbab@outlook.fr>
2021-06-19 16:09:02 +0200

    new commit

--- my_portfolio/my_portfolio/wsgi.py
+++ my_portfolio/my_portfolio/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_portfolio.settings')
 
 application = get_wsgi_application()
