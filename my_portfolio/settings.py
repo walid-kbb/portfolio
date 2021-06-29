652744ed7be8806beba479120fd354c4685aff82
Walid <walid-kebbab@outlook.fr>
2021-06-30 01:17:44 +0200

    new commit

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-yr7w*+ig3#v&f%ysm5+c1=^^(!*q)$**!1u@oldvdmu%rnu@-y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://walid-kebbab.herokuapp.com/']
+ALLOWED_HOSTS = ['https://walid-kebbab.herokuapp.com/','www.walid-kebbab.fr','walid-kebbab.fr']
 
 
 # Application definition
